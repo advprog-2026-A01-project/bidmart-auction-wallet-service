cfc959b9754e3811c5f803a7962ec807a5e7fd29
Nad-0 <go.nadine@cs.ui.ac.id>
2026-05-22 02:14:05 +0700

    feat: add gRPC service implementation for bidding

--- src/main/java/id/ac/ui/cs/advprog/auctionwallet/bidding/repository/BidRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/auctionwallet/bidding/repository/BidRepository.java
@@ -5,6 +5,7 @@ import id.ac.ui.cs.advprog.auctionwallet.bidding.model.Bid;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +15,6 @@ public interface BidRepository extends JpaRepository<Bid, Long> {
             Long auctionId,
             BidStatus status
     );
+
+    List<Bid> findByUserIdAndStatus(Long userId, BidStatus status);
 }
